dacce7e9c5d77bb8e09250bf3c426932ece14f6c
philippOkie <kriukphilipp@gmail.com>
2023-09-07 11:17:35 +0300

    add dom logic

--- src/index.js
+++ src/index.js
@@ -8,20 +8,24 @@ const feelsLikeSpan = document.getElementById("feelsLike");
 const humiditySpan = document.getElementById("humidity");
 const windSpan = document.getElementById("wind");
 const submit = document.getElementById("submit");
-const input = document.getElementById("findCity");
-const form = document.getElementById("form");
-
 const api = "https://api.weatherapi.com/v1/current.json?key=";
 const key = "55b3c11093064129922104657230409";
-const city = "kiev";
-const url = api + key + "&q=" + city;
+
 let farOrCelc = 0;
 // show feels like, condition.text, humidity, temp, wind, country and city!
-async function getWeather() {
+async function getWeather(api, key, input) {
+  const url = api + key + "&q=" + input;
   try {
     const response = await fetch(url, { mode: "cors" });
     const data = await response.json();
     console.log(data);
+    citySpan.innerText = data.location.name;
+    countrySpan.innerText = data.location.country;
+    conditionSpan.innerText = data.current.condition.text;
+    tempSpan.innerText = data.current.temp_c + " C";
+    feelsLikeSpan.innerText = "Feels like " + data.current.feelslike_c + " C";
+    humiditySpan.innerText = "Humidity " + data.current.humidity;
+    windSpan.innerText = data.current.gust_kph + " kph";
     if (!response.ok) {
       if (response.status === 400) {
         console.error(
@@ -36,4 +40,8 @@ async function getWeather() {
   }
 }
 
-getWeather();
+submit.addEventListener("click", () => {
+  const input = document.getElementById("findCity").value;
+  console.log(input);
+  getWeather(api, key, input);
+});
